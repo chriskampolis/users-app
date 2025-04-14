ea8b985070cd1bffbf890a945024af5a576ccd98
Chris Kampolis <chris.kampolis@outlook.com>
2025-04-14 21:18:44 +0300

    jest

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);
+router.get('/google/callback', authController.googleLogin);
 
 module.exports = router
+
+// https://accounts.google.com/o/oauth2/auth?client_id=407534761438-2saig0nu8c39u7qu3id08ruhec07830g.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
